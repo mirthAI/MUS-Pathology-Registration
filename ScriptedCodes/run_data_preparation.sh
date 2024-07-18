0aa3650ee15620616f7eed6de3749888bfcf52a1
Muhammad Imran <34628791+ImranNust@users.noreply.github.com>
2024-07-18 10:14:36 -0400

    Update run_data_preparation.sh

--- ScriptedCodes/run_data_preparation.sh
+++ ScriptedCodes/run_data_preparation.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 # Directories
-SOURCE_DIR="../data1/nifti_data"         # the dataset directory where nifti images are saved
-DESTINATION_DIR="../data1/png_data"          # Directory where png data will be stored
+SOURCE_DIR="../data/nifti_data"         # the dataset directory where nifti images are saved
+DESTINATION_DIR="../data/png_data"          # Directory where png data will be stored
 
 # Run the training script with the specified parameters
 python data_preparation.py \
